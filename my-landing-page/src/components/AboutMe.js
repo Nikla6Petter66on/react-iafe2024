ad2cf6a7d70fe14ee2e8cca0784c212066e3318b
Niklas Pettersson <61763372+Nikla6Petter66on@users.noreply.github.com>
2024-09-06 16:46:25 +0200

    clarity script

--- my-landing-page/src/components/AboutMe.js
+++ my-landing-page/src/components/AboutMe.js
@@ -64,9 +64,6 @@ const AboutMe = () => {
             </div>
           </div>
           <section className="flex justify-end p-8 text-black">
-        <a href="/contact" className="bg-yellow-400 text-black font-semibold py-3 px-6 rounded-md inline-flex items-center hover:bg-yellow-500 transition-colors duration-300">
-        Get in touch
-        </a>
       </section>
         </div>
       </section>
